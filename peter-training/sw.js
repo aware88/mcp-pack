ece70dd945497d827dc3ae2ca365d01c5d3baaa5
aware88 <tim.mak88@gmail.com>
2026-09-12 21:06:16 +0200

    Add Peter Training OS offline cache

---
+++ peter-training/sw.js
@@ -0,0 +1,5 @@
+const CACHE='peter-training-os-v3';
+const ASSETS=['./','./index.html','./app.css','./app1.js','./app2.js','./app3.js','./app4.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
